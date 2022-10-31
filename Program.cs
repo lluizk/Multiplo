4cc21d6f3b78de234f60ac2004f2f865ff74064a
sergio luiz <sergioj995@hotmail.com>
2022-10-31 17:22:47 -0300

    resolução do exercicio

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine(" ~~~~ Múltiplo ~~~~ ");
+
+System.Console.Write("Digite o número que deseja encontrar o multiplo: ");
+
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("Digite o número que deseja avaliar se é multiplo: ");
+
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1 % n2 == 0)
+{
+     
+     System.Console.WriteLine($"\n{n1} é multiplo de {n2}");
+}
+ else
+ { 
+    System.Console.WriteLine($"\n{n1} não é multiplo de {n2}");
+ }
+ 
